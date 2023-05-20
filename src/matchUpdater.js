a6ee86f73c608d78e7068ed52b6fed4bddb98f80
IQExplorers <iq.explorers@gmail.com>
2023-05-20 20:15:32 +0300

    first commit

---
+++ src/matchUpdater.js
@@ -0,0 +1,184 @@
+const { Client } = require("@notionhq/client");
+const parser = require("./parser");
+require("dotenv").config();
+
+const notion = new Client({
+  auth: process.env.INTEGRATION_TOKEN,
+});
+
+const databaseId = process.env.DATABASE_ID;
+
+async function getDatabaseMatches() {
+  const { results } = await notion.databases.query({ database_id: databaseId });
+
+  return results.map((page) => {
+    return { ...page.properties, pageId: page.id };
+  });
+}
+
+async function addMatches(matches) {
+  await Promise.all(
+    matches.map(async (match) => {
+      await notion.pages.create({
+        parent: { database_id: databaseId },
+        properties: {
+          Name: {
+            title: [
+              {
+                text: { content: match.Name ? match.Name : "NOT FOUND" },
+              },
+            ],
+          },
+          Status: {
+            status: {
+              name: match.Status ? match.Status : "NOT FOUND",
+            },
+          },
+          Game: {
+            select: { name: match.Game },
+          },
+          Format: {
+            rich_text: [
+              {
+                type: "text",
+                text: {
+                  content: match.Format ? match.Format : "NOT FOUND",
+                },
+              },
+            ],
+          },
+          Date: {
+            date: {
+              start: match.Date,
+            },
+          },
+          Tournament: {
+            rich_text: [
+              {
+                type: "text",
+                text: {
+                  content: match.Tournament
+                    ? match.Tournament.name
+                    : "NOT FOUND",
+                },
+              },
+            ],
+          },
+        },
+        children: [
+          {
+            object: "block",
+            type: "bookmark",
+            bookmark: {
+              url: match.Tournament ? match.Tournament.link : "NOT FOUND",
+            },
+          },
+        ],
+      });
+    })
+  );
+}
+
+function filterAddMatches(currentMatches, databasePages) {
+  return currentMatches.filter((match) => {
+    let fit = true;
+
+    databasePages.forEach((page) => {
+      if (match.Name === page.Name.title[0].text.content) {
+        fit = false;
+      }
+    });
+    return fit;
+  });
+}
+
+function filterDeprecatedMatches(currentMatches, databasePages) {
+  return databasePages.filter((page) => {
+    let deprecated = true;
+
+    currentMatches.forEach((match) => {
+      if (match.Name === page.Name.title[0].text.content) {
+        deprecated = false;
+      }
+    });
+
+    return deprecated;
+  });
+}
+
+async function deletePages(deprecatedPages) {
+  await Promise.all(
+    deprecatedPages.map(async (page) => {
+      await notion.pages.update({
+        page_id: page.pageId,
+        properties: {
+          Status: {
+            status: { name: "Done" },
+          },
+        },
+      });
+    })
+  );
+}
+
+async function updateDatabase(filteredCurrentMatches, databasePages) {
+  try {
+    const updates = [];
+    for (const match of filteredCurrentMatches) {
+      for (const page of databasePages) {
+        const pageToUpdate = {};
+        if (
+          match.Name === page.Name.title[0].text.content &&
+          match.Status !== page.Status.status.name
+        ) {
+          pageToUpdate.Status = { status: { name: match.Status } };
+        }
+
+        if (
+          match.Name === page.Name.title[0].text.content &&
+          match.Date.getTime() !== Date.parse(page.Date.date.start)
+        ) {
+          pageToUpdate.Date = { date: { start: match.Date } };
+        }
+
+        if (Object.keys(pageToUpdate).length > 0) {
+          updates.push(
+            await notion.pages.update({
+              page_id: page.pageId,
+              properties: pageToUpdate,
+            })
+          );
+        }
+      }
+    }
+    await Promise.all(updates);
+  } catch (ex) {
+    console.log("ex", ex);
+  }
+}
+
+async function run() {
+  const currentMatches = await parser.getMatches();
+  const databasePages = await getDatabaseMatches();
+  const filteredCurrentMatches = filterAddMatches(
+    currentMatches,
+    databasePages
+  );
+  const deprecatedPages = filterDeprecatedMatches(
+    currentMatches,
+    databasePages
+  );
+
+  updateDatabase(currentMatches, databasePages);
+  deletePages(deprecatedPages);
+  addMatches(filteredCurrentMatches);
+  //change
+}
+
+run();
+
+// const intervalId = setInterval(run, 60000);
+
+// setTimeout(() => {
+//   clearInterval(intervalId);
+// }, 360000);
---
+++ src/parser.js
@@ -0,0 +1,169 @@
+const cheerio = require("cheerio");
+const axios = require("axios");
+const url = require("url");
+const { table } = require("console");
+
+const CSGOSelector =
+  "#mw-content-text > div > div.panel-box.wiki-bordercolor-light.toggle-area.toggle-area-1.matches-list > div:nth-child(2) > div:nth-child(1)";
+
+const valorantSelector =
+  "#mw-content-text > div > div.panel-box.wiki-bordercolor-light.toggle-area.toggle-area-1.matches-list > div:nth-child(2) > div:nth-child(1)";
+
+const dotaSelector =
+  "#mw-content-text > div > div.panel-box.wiki-bordercolor-light.toggle-area.toggle-area-1.matches-list > div:nth-child(2) > div:nth-child(1)";
+
+//fake
+const fakeDotaSelector = "#mw-content-text > div > div:nth-child(3) > div";
+
+const CSGOSourse = "https://liquipedia.net/counterstrike/Liquipedia:Matches";
+
+const dotaSource =
+  "https://liquipedia.net/dota2/Liquipedia:Upcoming_and_ongoing_matches";
+//fake
+const fakeDotaSource = "https://liquipedia.net/dota2/B8";
+
+const valorantSourse = "https://liquipedia.net/valorant/Liquipedia:Matches";
+
+async function parseMatches(sourcePage, selector, game) {
+  const { data: html } = await axios.get(sourcePage);
+
+  let matches = [];
+  const $ = cheerio.load(html);
+  $(selector)
+    .find("table")
+    .each((tableId, tableElement) => {
+      let enemy;
+      let matchFound = false;
+
+      const leftTeam = $(tableElement).find(".team-left").text().trim();
+      const rightTeam = $(tableElement).find(".team-right").text().trim();
+
+      if (leftTeam === "B8") {
+        enemy = rightTeam;
+        matchFound = true;
+      } else if (rightTeam === "B8") {
+        enemy = leftTeam;
+        matchFound = true;
+      }
+
+      if (matchFound) {
+        const tournamentName = $(tableElement)
+          .find("tr:nth-child(2) > td > div > div > a")
+          .text();
+
+        const tournamentLink = $(tableElement)
+          .find("tr:nth-child(2) > td > div > div > a")
+          .attr("href");
+
+        const dateText = $(tableElement)
+          .find(".match-countdown")
+          .text()
+          .replace("-", "")
+          .trim();
+
+        const format = $(tableElement)
+          .find("tr:nth-child(1)")
+          .find("abbr")
+          .text();
+
+        const dateObj = new Date(Date.parse(dateText));
+
+        const status = dateObj.getTime() < Date.now() ? "going" : "upcoming";
+
+        matches.push({
+          Name: enemy,
+          Game: game,
+          Date: dateObj,
+          Format: format,
+          Status: status,
+          Tournament: {
+            name: tournamentName,
+            link: "https://liquipedia.net" + tournamentLink,
+          },
+        });
+        matchFound = false;
+        enemy = null;
+      }
+    });
+  console.log(`${game}`, matches);
+  return matches;
+}
+
+async function parseDota() {
+  const { data: html } = await axios.get(fakeDotaSource);
+
+  let matches = [];
+  const $ = cheerio.load(html);
+  $(fakeDotaSelector)
+    .find("table")
+    .each((tableId, tableElement) => {
+      const leftTeam = $(tableElement).find(".team-left").text().trim();
+      const rightTeam = $(tableElement).find(".team-right").text().trim();
+
+      const dateText = $(tableElement)
+        .find(".match-countdown")
+        .text()
+        .replace("-", "")
+        .trim();
+
+      const dateObj = new Date(Date.parse(dateText));
+
+      const tournamentName = $(tableElement)
+        .find("tr:nth-child(2) > td > div > div > a")
+        .text();
+
+      const tournamentLink = $(tableElement)
+        .find("tr:nth-child(2) > td > div > div > a")
+        .attr("href");
+
+      const format = $(tableElement)
+        .find("tr:nth-child(1)")
+        .find("abbr")
+        .text();
+
+      const status = dateObj.getTime() < Date.now() ? "going" : "upcoming";
+
+      const enemy = leftTeam === "B8" ? rightTeam : leftTeam;
+
+      matches.push({
+        Name: enemy,
+        Game: "dota",
+        Date: dateObj,
+        Format: format,
+        Status: status,
+        Tournament: {
+          name: tournamentName,
+          link: "https://liquipedia.net" + tournamentLink,
+        },
+      });
+    });
+  console.log("dota", matches);
+  return matches;
+}
+
+async function getMatches() {
+  const valorantMatches =  await parseMatches(
+    valorantSourse,
+    valorantSelector,
+    "valorant"
+  );
+  const CSGOMathes = await parseMatches(CSGOSourse, CSGOSelector, "csgo");
+  const dotaMatches = await parseDota();
+
+  return [...CSGOMathes, ...valorantMatches, ...dotaMatches];
+}
+
+// parseMatches(valorantSourse, valorantSelector, "valorant");
+// parseMatches(CSGOSourse, CSGOSelector, "csgo");
+//parseMatches(dotaSource, dotaSelector, "dota");
+// parseDota();
+
+// const intervalId = setInterval(parseCS, 10000);
+
+// setTimeout(() => {
+//   clearInterval(intervalId);
+// }, 30000);
+
+module.exports = {
+  getMatches: getMatches,
+};
